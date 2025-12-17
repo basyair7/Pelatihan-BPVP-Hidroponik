55e14bdd088c135bd20d09b725798397b6e301a1
basyair7 <basyair.fathul@gmail.com>
2025-12-17 10:14:25 +0700

    First Commit Program

--- Otomatis_Nutrisi_Hidroponik_Batch_V/config.h
+++ Otomatis_Nutrisi_Hidroponik_Batch_V/config.h
@@ -11,22 +11,22 @@
 #define PASS_STA             "elektronika@2025"
 
 /****** DHT SENSOR *******/
-#define DHTPIN      2
-#define DHTTYPE     DHT11
+#define DHTPIN      2         // Pin Input DHT
+#define DHTTYPE     DHT11     // DHT22, DHT21, DHT11
 
 /******* DS18B20 *******/
-#define ONE_WIRE_BUS  4 // pininput DS18B20
+#define ONE_WIRE_BUS  4       // Pin Input DS18B20
 
 /******* TDS SENSOR *******/
-#define TdsSensorPin    34
+#define TdsSensorPin    34    // Pin Input TDS Sensor
 #define VREF            3.3   // analog reference voltage of the ADC
 #define ADC_RESOLUTION  4096  // for ESP 4096, for arduino 1023
 #define SCOUNT          10    // sum of sample point
 
 /******* RELAY & PUSH BUTTON *******/
-#define RELAY1        25  // Relay pompa A atau pompa 1
-#define RELAY2        26  // Relay pompa B atau pompa 2
-#define startButton   32  // Tombol mulai sistem
-#define resetButton   33  // Tombol reset sistem
+#define RELAY1        25      // Pin Output Relay A / Pump 1
+#define RELAY2        26      // Pin Output Relay B / Pump 2
+#define startButton   32      // Pin Input Start Button Sistem
+#define resetButton   33      // Pin Input Reset Button Sistem
 #define relayA        RELAY1
 #define relayB        RELAY2
